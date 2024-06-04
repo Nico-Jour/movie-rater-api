19a0bcce49c7c9b3a42b0e825bd8eae37b368635
Monoceros-frontend <monoceros.frontend@gmail.com>
2024-06-04 18:10:14 +0200

    delete test files

--- src/omdbApi/omdbApi.service.ts
+++ src/omdbApi/omdbApi.service.ts
@@ -12,20 +12,8 @@ export class OmdbApiService {
       baseURL: 'http://www.omdbapi.com/',
       timeout: 5000,
       headers: { 'Content-Type': 'application/json' },
+      params: { apikey: OMDB_API_KEY },
     });
-
-    this.axiosInstance.interceptors.request.use(
-      (config) => {
-        if (!config.params) {
-          config.params = {};
-        }
-        config.params['apikey'] = OMDB_API_KEY;
-        return config;
-      },
-      (error) => {
-        return Promise.reject(error);
-      },
-    );
   }
 
   async get<T>(url: string, params?: any): Promise<T> {
